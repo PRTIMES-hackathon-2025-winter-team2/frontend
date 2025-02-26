89207cac15eeac853e1644728f785dc39c8c8460
yutoAb <yuto27abe@gmail.com>
2025-02-26 17:19:11 +0900

    枝の位置調整

--- src/components/pages/tree/TreeComponent.tsx
+++ src/components/pages/tree/TreeComponent.tsx
@@ -42,12 +42,12 @@ export const TreeComponent = () => {
       />
       {/* Curved Branches */}
       {[
-        { topOffset: -400, leftOffset: -30, rotate: -30 },
+        { topOffset: -400, leftOffset: -70, rotate: -30 },
         { topOffset: -370, leftOffset: 30, rotate: 30 },
-        { topOffset: -340, leftOffset: -50, rotate: -45 },
-        { topOffset: -310, leftOffset: 50, rotate: 45 },
+        { topOffset: -340, leftOffset: -70, rotate: -45 },
+        { topOffset: -310, leftOffset: 40, rotate: 45 },
         { topOffset: -280, leftOffset: -70, rotate: -60 },
-        { topOffset: -250, leftOffset: 70, rotate: 60 },
+        { topOffset: -250, leftOffset: 40, rotate: 60 },
       ].map((branch, index) => (
         <motion.div
           key={index}
