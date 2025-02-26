768b49f85fb53a2a11595ac84a96af42484562af
yutoAb <yuto27abe@gmail.com>
2025-02-26 15:26:42 +0900

    テンプレート作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MainComponent } from "./components/pages/MainComponents";
+import { NotFound } from "./components/pages/NotFound";
 
 function App() {
+  const ENDPOINT_BASE_PATH = "";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename={`${ENDPOINT_BASE_PATH}`}>
+      <Routes>
+        <Route path="">
+          <Route index element={<MainComponent />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
