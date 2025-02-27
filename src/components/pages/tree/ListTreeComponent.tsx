d644b4188f1962142cc21fa80d79642d35a3945a
yutoAb <yuto27abe@gmail.com>
2025-02-28 00:55:45 +0900

    途中

--- src/components/pages/tree/ListTreeComponent.tsx
+++ src/components/pages/tree/ListTreeComponent.tsx
@@ -1,4 +1,11 @@
-import { Container, Stack, Typography, Button, Box } from "@mui/material";
+import {
+  Container,
+  Stack,
+  Typography,
+  Button,
+  Box,
+  Grid2 as Grid,
+} from "@mui/material";
 import { useTreeList } from "../hooks/useTreeList";
 import { useParams, Link } from "react-router-dom";
 import dream_tree from "../assets/dream_tree.png";
@@ -10,22 +17,70 @@ export const ListTreeComponent = () => {
 
   return (
     <Container>
-      <Stack direction="column" spacing={4} alignItems="center" sx={{ paddingTop: 15 }}>
+      <Stack
+        direction="column"
+        spacing={4}
+        alignItems="center"
+        sx={{ paddingTop: 15 }}
+      >
         <Typography variant="h3" sx={{ mt: 3 }}>
           作成した夢の木一覧
         </Typography>
-        <Box
-          component="img"
-          src={dream_tree}
-          alt="dream_tree"
-          sx={{
-            width: "100px",
-            height: "auto",
-            objectFit: "contain",
-            transition: "width 0.3s ease-in-out",
-          }}
-        />
       </Stack>
+      <Grid container spacing={2}>
+        <Grid size={{ xs: 6, md: 8 }}>
+          <Box
+            component="img"
+            src={dream_tree}
+            alt="dream_tree"
+            sx={{
+              width: "100px",
+              height: "auto",
+              objectFit: "contain",
+              transition: "width 0.3s ease-in-out",
+            }}
+          />
+        </Grid>
+        <Grid size={{ xs: 6, md: 4 }}>
+          <Box
+            component="img"
+            src={dream_tree}
+            alt="dream_tree"
+            sx={{
+              width: "100px",
+              height: "auto",
+              objectFit: "contain",
+              transition: "width 0.3s ease-in-out",
+            }}
+          />
+        </Grid>
+        <Grid size={{ xs: 6, md: 4 }}>
+          <Box
+            component="img"
+            src={dream_tree}
+            alt="dream_tree"
+            sx={{
+              width: "100px",
+              height: "auto",
+              objectFit: "contain",
+              transition: "width 0.3s ease-in-out",
+            }}
+          />
+        </Grid>
+        <Grid size={{ xs: 6, md: 8 }}>
+          <Box
+            component="img"
+            src={dream_tree}
+            alt="dream_tree"
+            sx={{
+              width: "100px",
+              height: "auto",
+              objectFit: "contain",
+              transition: "width 0.3s ease-in-out",
+            }}
+          />
+        </Grid>
+      </Grid>
       <Button
         variant="contained"
         sx={{
