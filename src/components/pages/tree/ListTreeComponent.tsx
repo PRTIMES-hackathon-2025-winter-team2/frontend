e6e9da9c3aca195c4d73e88c4bfba085f2bbe641
yutoAb <yuto27abe@gmail.com>
2025-02-28 00:14:41 +0900

    リスト修正

--- src/components/pages/tree/ListTreeComponent.tsx
+++ src/components/pages/tree/ListTreeComponent.tsx
@@ -1,5 +1,37 @@
-import { Box } from "@mui/material";
+import { Container, Stack, Typography, Button } from "@mui/material";
 
 export const ListTreeComponent = () => {
-  return <Box>List</Box>;
+  return (
+    <Container>
+      <Stack direction="column" spacing={4} alignItems="center">
+        <Typography variant="h3">List</Typography>
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: "pink",
+            "&:hover": { backgroundColor: "hotpink" },
+            width: "250px", // ボタンの幅を固定
+            maxWidth: "100%", // レスポンシブ対応
+          }}
+          // component={Link}
+          // to={`/input/${userId}`}
+        >
+          Dream Tree 作成画面へ
+        </Button>
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: "pink",
+            "&:hover": { backgroundColor: "hotpink" },
+            width: "250px", // ボタンの幅を固定
+            maxWidth: "100%", // レスポンシブ対応
+          }}
+          // component={Link}
+          // to={`/list/${userId}`}
+        >
+          Dream Tree 閲覧画面へ
+        </Button>
+      </Stack>
+    </Container>
+  );
 };
