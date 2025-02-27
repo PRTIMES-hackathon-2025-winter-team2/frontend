5857049eb205936fa97784cc637c071fffed90a9
yutoAb <yuto27abe@gmail.com>
2025-02-27 18:41:40 +0900

    途中経過

--- src/components/pages/MainComponents.tsx
+++ src/components/pages/MainComponents.tsx
@@ -1,9 +1,23 @@
-import { Box, Container } from "@mui/material";
+import { Box, Container, Button } from "@mui/material";
+import { Link, useParams } from "react-router-dom";
 
 export const MainComponent = () => {
+  const { userId } = useParams();
+  console.log(userId);
   return (
     <Container sx={{ mt: 2 }}>
       <Box>Main Page</Box>
+      <Button
+        variant="contained"
+        sx={{
+          backgroundColor: "pink", // ボタンの背景色
+          "&:hover": { backgroundColor: "hotpink" }, // ホバー時の背景色
+        }}
+        component={Link}
+        to={`/input/${userId}/treeId`}
+      >
+        Dream Tree 作成画面へ
+      </Button>
     </Container>
   );
 };
