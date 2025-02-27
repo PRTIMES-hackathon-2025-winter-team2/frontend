dceb23e925ff3b8051a0e792cb32d61578016cd1
yutoAb <yuto27abe@gmail.com>
2025-02-27 14:36:30 +0900

    モックデータの作成

---
+++ src/components/pages/models/mockData.ts
@@ -0,0 +1,52 @@
+import { Dream } from "./Dream";
+
+export const DreamData: Dream[] = [
+  {
+    id: "id1",
+    tree_id: "treeId1",
+    position: 0,
+    title: "メジャーからオファーが欲しい！",
+    created_at: "2025-02-27",
+    ended_at: "2025-02-27",
+  },
+  {
+    id: "id1",
+    tree_id: "treeId1",
+    position: 1,
+    title: "ドバイに行きたい！",
+    created_at: "2025-02-27",
+    ended_at: "2025-02-27",
+  },
+  {
+    id: "id1",
+    tree_id: "treeId1",
+    position: 2,
+    title: "宇宙飛行士になりたい！",
+    created_at: "2025-02-27",
+    ended_at: "2025-02-27",
+  },
+  {
+    id: "id1",
+    tree_id: "treeId1",
+    position: 3,
+    title: "お金持ちになりたい！",
+    created_at: "2025-02-27",
+    ended_at: "2025-02-27",
+  },
+  {
+    id: "id1",
+    tree_id: "treeId1",
+    position: 4,
+    title: "MIU MIUのバッグが欲しい",
+    created_at: "2025-02-27",
+    ended_at: "2025-02-27",
+  },
+  {
+    id: "id1",
+    tree_id: "treeId1",
+    position: 5,
+    title: "定期テストでいい点をとる！",
+    created_at: "2025-02-27",
+    ended_at: "2025-02-27",
+  },
+];
