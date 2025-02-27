65ae1ed08967d1f04bb5777496953a5c89ca3a23
yutoAb <yuto27abe@gmail.com>
2025-02-27 18:50:06 +0900

    MyPage 修正

--- src/components/pages/MainComponents.tsx
+++ src/components/pages/MainComponents.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Button } from "@mui/material";
+import { Typography, Container, Button, Stack } from "@mui/material";
 import { Link, useParams } from "react-router-dom";
 
 export const MainComponent = () => {
@@ -6,18 +6,31 @@ export const MainComponent = () => {
   console.log(userId);
   return (
     <Container sx={{ mt: 2 }}>
-      <Box>Main Page</Box>
-      <Button
-        variant="contained"
-        sx={{
-          backgroundColor: "pink", // ボタンの背景色
-          "&:hover": { backgroundColor: "hotpink" }, // ホバー時の背景色
-        }}
-        component={Link}
-        to={`/input/${userId}`}
-      >
-        Dream Tree 作成画面へ
-      </Button>
+      <Stack direction="column" spacing={2}>
+        <Typography variant="h2">My Page</Typography>
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: "pink", // ボタンの背景色
+            "&:hover": { backgroundColor: "hotpink" }, // ホバー時の背景色
+          }}
+          component={Link}
+          to={`/input/${userId}`}
+        >
+          Dream Tree 作成画面へ
+        </Button>
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: "pink", // ボタンの背景色
+            "&:hover": { backgroundColor: "hotpink" }, // ホバー時の背景色
+          }}
+          component={Link}
+          to={`/trees/${userId}/treeId`}
+        >
+          Dream Tree 閲覧画面へ
+        </Button>
+      </Stack>
     </Container>
   );
 };
