e98e394a01d6a5b67c6cbc5469d1a72eae3aca2b
yutoAb <yuto27abe@gmail.com>
2025-02-28 00:46:32 +0900

    マイページ修正

--- src/components/pages/MainComponents.tsx
+++ src/components/pages/MainComponents.tsx
@@ -1,4 +1,4 @@
-import { Typography, Container, Button, Stack } from "@mui/material";
+import { Typography, Container, Button, Stack, Box } from "@mui/material";
 import { Link, useParams } from "react-router-dom";
 import { useUserInfo } from "./hooks/useUserInfo";
 
@@ -7,36 +7,43 @@ export const MainComponent = () => {
   const userInfo = useUserInfo(userId || "");
 
   return (
-    <Container sx={{ mt: 6 }}>
-      <Stack direction="column" spacing={4} alignItems="center">
-        <Typography variant="h3">{userInfo.name}さんのマイページ</Typography>
-        <Button
-          variant="contained"
-          sx={{
-            backgroundColor: "pink",
-            "&:hover": { backgroundColor: "hotpink" },
-            width: "250px", // ボタンの幅を固定
-            maxWidth: "100%", // レスポンシブ対応
-          }}
-          component={Link}
-          to={`/input/${userId}`}
+    <Box sx={{ background: "#d4f4a1", width: "100%", height: "100vh" }}>
+      <Container>
+        <Stack
+          direction="column"
+          spacing={4}
+          alignItems="center"
+          sx={{ paddingTop: 15 }}
         >
-          Dream Tree 作成画面へ
-        </Button>
-        <Button
-          variant="contained"
-          sx={{
-            backgroundColor: "pink",
-            "&:hover": { backgroundColor: "hotpink" },
-            width: "250px", // ボタンの幅を固定
-            maxWidth: "100%", // レスポンシブ対応
-          }}
-          component={Link}
-          to={`/list/${userId}`}
-        >
-          Dream Tree 閲覧画面へ
-        </Button>
-      </Stack>
-    </Container>
+          <Typography variant="h3">{userInfo.name}さんのマイページ</Typography>
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: "pink",
+              "&:hover": { backgroundColor: "hotpink" },
+              width: "250px", // ボタンの幅を固定
+              maxWidth: "100%", // レスポンシブ対応
+            }}
+            component={Link}
+            to={`/input/${userId}`}
+          >
+            Dream Tree 作成画面へ
+          </Button>
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: "pink",
+              "&:hover": { backgroundColor: "hotpink" },
+              width: "250px", // ボタンの幅を固定
+              maxWidth: "100%", // レスポンシブ対応
+            }}
+            component={Link}
+            to={`/list/${userId}`}
+          >
+            Dream Tree 閲覧画面へ
+          </Button>
+        </Stack>
+      </Container>
+    </Box>
   );
 };
